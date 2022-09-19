550cb04739f9909c1ba382fc0c47c5e8bd2e879c
ACER <manirocks43@gmail.com>
2022-09-18 20:14:52 -0400

    Shebang topic

--- Shebang.sh
+++ Shebang.sh
@@ -2,3 +2,5 @@
 # Completely optional
 # adding that willhave an understanding of what the script is
 # best method is <shell> <script>.
+
+# Shell script is nothing but adding all your manual commands in an order.
